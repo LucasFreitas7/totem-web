dd59fb4f210dd097c2185cea3b3a52462278769b
Lucas Andrade Freitas <lucas.freitas2@aluno.ufp.edu.br>
2021-07-02 16:27:56 -0300

    api atualizada, com mais recursos

---
+++ src/backend-py/ServicoDAO.py
@@ -0,0 +1,45 @@
+
+
+class Servicos:
+    def __init__(self, sCNPJCliente, sCNPJParceiro, dValor: float, iCupom: int, iLeitor: int):
+        self.sCNPJCliente = sCNPJCliente
+        self.sCNPJParceiro = sCNPJParceiro
+        self.dValor = dValor
+        self.iCupom = iCupom
+        self.iLeitor = iLeitor
+
+    def __str__(self):
+        return f"{self.sCNPJCliente}/{self.sCNPJParceiro}/{self.dValor}/{self.iCupom}/{self.iLeitor}"
+
+    def __repr__(self):
+        return (
+            f"CPNJ Cliente: {self.sCNPJCliente}\n"
+            f"CNPJ Parceiro: {self.sCNPJParceiro}\n"
+            f"Valor: {self.dValor}\n"
+            f"Cupom: {self.iCupom}\n"
+            f"Leitor: {self.iLeitor}"    
+        )
+
+
+
+class Parcelas:
+    def __init__(self, sCNPJCliente, sCNPJParceiro, iParcelas: int, dValor: float, iCupom: int, iLeitor: int):
+        self.sCNPJCliente = sCNPJCliente
+        self.sCNPJParceiro = sCNPJParceiro
+        self.iParcelas = iParcelas
+        self.dValor = dValor
+        self.iCupom = iCupom
+        self.iLeitor = iLeitor
+
+    def __str__(self):
+        return f"{self.sCNPJCliente}/{self.sCNPJParceiro}/{self.iParcelas}{self.dValor}//{self.iCupom}/{self.iLeitor}"
+
+    def __repr__(self):
+        return (
+            f"CPNJ Cliente: {self.sCNPJCliente}\n"
+            f"CNPJ Parceiro: {self.sCNPJParceiro}\n"
+            f"Parcelas: {self.iParcelas}\n"
+            f"Valor: {self.dValor}\n"
+            f"Cupom: {self.iCupom}\n"
+            f"Leitor: {self.iLeitor}"    
+        )
--- src/backend-py/routes.py
+++ src/backend-py/routes.py
@@ -1,17 +1,16 @@
 from flask import Flask, jsonify, request
 from ctypes import *
+from ServicoDAO import *
 from flask_cors import CORS
 import ctypes
 
-
+servico =  Servicos("60177876000130", "61492096000147", 30, 1, 1)
+parcelas = Parcelas("60177876000130", "61492096000147", 2,  30, 1, 1)
 lib = WinDLL(r".\\TefClientmc.dll")
 
 ctypes.c_wchar_p()
-sCNPJCliente = "60177876000130"
-sCNPJParceiro = "61492096000147"
-new_sCNPJCliente = sCNPJCliente.encode('utf-8')
-new_sCNPJParceiro = sCNPJParceiro.encode('utf-8')
-
+new_sCNPJCliente = servico.sCNPJCliente.encode('utf-8')
+new_sCNPJParceiro = servico.sCNPJParceiro.encode('utf-8')
 #var = lib.VendeCredito(ctypes.c_char_p(new_sCNPJCliente),ctypes.c_char_p(new_sCNPJParceiro),c_double(30.5),c_int(1), c_int(1))
 
 #  print(f"{lib.VendeCredito}")
@@ -23,10 +22,83 @@ CORS(app)
 def requisicaoRecarga():
         try:
             request.headers['acesso']
-            lib.RecargaCelular(ctypes.c_char_p(new_sCNPJCliente),ctypes.c_char_p(new_sCNPJParceiro),c_int(1), c_int(1))
-            print("Requisição para chamar a dll")
-            return jsonify({'status' : "Processado com sucesso"});
+            lib.RecargaCelular(ctypes.c_char_p(new_sCNPJCliente),ctypes.c_char_p(new_sCNPJParceiro),c_int(servico.iCupom), c_int(servico.iLeitor))
+            servico.iCupom + 1
+            print("Requisição para chamar a Recarga")
+            return jsonify({'status' : "Recarga processada com sucesso"})
         except:
-            return jsonify({'status' : "Erro no processamento"});
+            return jsonify({'status' : "Erro no processamento"})
    
+
+
+@app.route("/vendecredito", methods = ["GET"])
+
+def requisicaoVendeCredito():
+        try:
+            request.headers['acesso']
+            lib.VendeCredito(ctypes.c_char_p(new_sCNPJCliente),ctypes.c_char_p(new_sCNPJParceiro),c_double(servico.dValor), c_int(servico.iCupom), c_int(servico.iLeitor))
+            servico.iCupom + 1
+            print("Requisição para chamar a venda credito")
+            return jsonify({'status' : "Venda credito processado com sucesso"})
+        except:
+            return jsonify({'status' : "Erro no processamento"})
+
+@app.route("/vendedebito", methods = ["GET"])
+
+def requisicaoVendeDebito():
+        try:
+            request.headers['acesso']
+            lib.VendeDebito(ctypes.c_char_p(new_sCNPJCliente),ctypes.c_char_p(new_sCNPJParceiro),c_double(servico.dValor), c_int(servico.iCupom), c_int(servico.iLeitor))
+            servico.iCupom + 1
+            print("Requisição para chamar a venda debito")
+            return jsonify({'status' : "Venda debito processado com sucesso"})
+        except:
+            return jsonify({'status' : "Erro no processamento"})
+        
+@app.route("/vendecreditoparcelado", methods = ["GET"])
+
+def vendeCreditoParcelado():
+        try:
+            request.headers['acesso']
+            lib.VendeCreditoParcLoja(ctypes.c_char_p(new_sCNPJCliente),ctypes.c_char_p(new_sCNPJParceiro),c_int(parcelas.iParcelas), c_double(servico.dValor), c_int(servico.iCupom), c_int(servico.iLeitor))
+            servico.iCupom + 1
+            print("Requisição para chamar a venda credito parcelado")
+            return jsonify({'status' : "Venda credito parcelado processado com sucesso"})
+        except:
+            return jsonify({'status' : "Erro no processamento"})
+
+@app.route("/cancelar", methods = ["GET"])
+
+def requisicaoCancelamento():
+        try:
+            request.headers['acesso']
+            lib.Cancelar(ctypes.c_char_p(new_sCNPJCliente),ctypes.c_char_p(new_sCNPJParceiro), c_double(servico.dValor))
+            print("Requisição para cancelamento")
+            return jsonify({'status' : "Cancelamento processado com sucesso"})
+        except:
+            return jsonify({'status' : "Erro no processamento"})
+
+
+@app.route("/reimpressao", methods = ["GET"])
+
+def requisicaoReimpressao():
+        try:
+            request.headers['acesso']
+            lib.Reimpressao(ctypes.c_char_p(new_sCNPJCliente),ctypes.c_char_p(new_sCNPJParceiro),c_double(servico.dValor), c_int(servico.iCupom), c_int(1))
+            print("Requisição para reimpressao")
+            return jsonify({'status' : "Reimpressao processado com sucesso"})
+        except:
+            return jsonify({'status' : "Erro no processamento"})
+
+@app.route("/relatorio", methods = ["GET"])
+
+def requisicaoRelatorio():
+        try:
+            request.headers['acesso']
+            lib.Relatorio(ctypes.c_char_p(new_sCNPJCliente),ctypes.c_char_p(new_sCNPJParceiro), c_int(servico.iCupom), c_int(1))
+            print("Requisição para relatorio")
+            return jsonify({'status' : "Relatorio processado com sucesso"})
+        except:
+            return jsonify({'status' : "Erro no processamento"})
+
 app.run()
